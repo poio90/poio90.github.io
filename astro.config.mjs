1259fab3f1963c62d03590287ebfc6d83a25f0ad
German Vargas <germanv09@outlook.com>
2023-12-20 16:40:19 -0300

    config to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,25 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+
+const SERVER_PORT = 3000;
+// the url to access your blog during local development
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+// the url to access your blog after deploying it somewhere (Eg. Netlify)
+const LIVE_URL = "https://poio90.github.io";
+// this is the astro command your npm script runs
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL;
+// When you're building your site in local or in CI, you could just set your URL manually
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://astronaut.github.io',
-  // base: '/mi-repo',
+  server: { port: SERVER_PORT },
+  site: BASE_URL,
   experimental: {
     viewTransitions: true,
   },
